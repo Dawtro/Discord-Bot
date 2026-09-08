ce2a0294aa98866eea026398cc6ec692765d2b22
Dawtro <dawtro789@gmail.com>
2026-09-08 18:48:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -540,7 +540,7 @@ async function updateStatusMessage() {
             .addSeparatorComponents(new SeparatorBuilder().setDivider(true))
             .addTextDisplayComponents(
                 new TextDisplayBuilder().setContent(
-                    `*ER:LC status • Updates every 45 seconds • Last updated: <t:${Math.floor(statusUpdatedAt / 1000)}:f>*`
+                    `*ER:LC status • Updates every 45 seconds • Last updated: <t:${Math.floor(statusUpdatedAt / 1000)}:t>*`
                 )
             );
 
